606fe2941f7ac3e765ac493e9348b159677913d8
Paul van der Meijs <paul@lowtone.nl>
2013-06-24 09:18:30 +0200

    Added filters for thumbnails.

--- lowtone-media-video.php
+++ lowtone-media-video.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Video
  * Plugin URI: http://wordpress.lowtone.nl/media-video
- * Description: Better support for video.
+ * Description: Imrpoved support for video.
  * Version: 1.0
  * Author: Lowtone <info@lowtone.nl>
  * Author URI: http://lowtone.nl
@@ -34,11 +34,10 @@ namespace lowtone\media\video {
 				Video::__register();
 				
 				add_shortcode("video", function($atts, $content) {
-					if (isset($atts["id"])) {
+					if (isset($atts["id"])) 
 						$video = Video::findById($atts["id"]);
-					} else if ($content) {
+					else if ($content) 
 						$video = new Video();
-					}
 
 					if (!isset($video))
 						return '<!-- Invalid video resource -->';
@@ -53,6 +52,56 @@ namespace lowtone\media\video {
 				add_filter("media_send_to_editor", function($html, $id, $attachment) {
 					return sprintf('[video id=%s /]', $id);
 				}, 10, 3);
+
+				// Return poster
+				
+				$__disableDownsize = false;
+				
+				add_filter("image_downsize", function($image, $postId, $size) use (&$__disableDownsize) {
+					if ($__disableDownsize)
+						return $image;
+
+					$post = get_post($postId);
+
+					if (!preg_match("#^video\/#", $post->{Video::PROPERTY_POST_MIME_TYPE}))
+						return $image;
+
+					if (!($thumbnailId = get_post_thumbnail_id($postId)))
+						return $image;
+
+					$__disableDownsize = true;
+
+					$ret = image_downsize($thumbnailId, $size);
+
+					$__disableDownsize = false;
+
+					return $ret;
+				}, 0, 3);
+
+				// Post thumbnail
+				
+				$__disablePostThumbnail = false;
+				
+				add_filter("post_thumbnail_html", function($html, $postId, $thumbnailId, $size, $attr) use (&$__disablePostThumbnail) {
+					if ($__disablePostThumbnail)
+						return $html;
+					
+					$thumbnail = get_post($thumbnailId);
+
+					if (!preg_match("#^video\/#", $thumbnail->{Video::PROPERTY_POST_MIME_TYPE}))
+						return $html;
+
+					if (is_singular() && ($video = Video::fromPost($thumbnail)))
+						return $video->player();
+
+					$__disablePostThumbnail = true;
+
+					$html = get_the_post_thumbnail($thumbnailId, $size, $attr);
+
+					$__disablePostThumbnail = false;
+
+					return $html;
+				});
 
 				// Register textdomain
 				
